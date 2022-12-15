1b6922561c7e50571060cac3b470b6a5c9359bf8
julianamsousa <juliana.ms@outlook.com>
2022-12-14 23:24:24 -0300

    Atualizações

---
+++ readme.md
@@ -0,0 +1,15 @@
+#### Desafio: Explorando o Rest Assured para a Automação de Testes de API  #### 
+
+Repositório contém desafio de projeto do Bootcamp da GFT Quality Assurance para mulheres.
+___
+**Como funciona o projeto:**
+
+Endpoint's
+
+#1 - Realiza a busca de todos os Ids do Booking.
+#2 - 
+
+
+
+
+
--- src/main/java/Entities/User.java
+++ src/main/java/Entities/User.java
@@ -2,11 +2,19 @@ package Entities;
 
 public class User {
     private String username;
-    private String firstname;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
 
-    public User(String username, String firstname) {
+    public User(String username, String firstName, String lastName, String email, String password, String phone) {
         this.username = username;
-        this.firstname = firstname;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
     }
 
     public String getUsername() {
@@ -17,11 +25,44 @@ public class User {
         this.username = username;
     }
 
-    public String getFirstname() {
-        return firstname;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
 }
